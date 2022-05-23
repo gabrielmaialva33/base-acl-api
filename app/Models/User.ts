5ef852201377422ab9d7af1b0a9596aeb7c65292
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-05-22 23:30:36 -0300

    chore: configure adonis auth

---
+++ app/Models/User.ts
@@ -0,0 +1,72 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  public static table = 'users'
+
+  /**
+   * ------------------------------------------------------
+   * Columns
+   * ------------------------------------------------------
+   * - column typing struct
+   */
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public first_name: string
+
+  @column()
+  public last_name: string
+
+  @column()
+  public username: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public remember_me_token?: string
+
+  @column.dateTime({ autoCreate: true, serializeAs: null })
+  public created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  public updated_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
+  public deleted_at: DateTime
+
+  /**
+   * ------------------------------------------------------
+   * Hooks
+   * ------------------------------------------------------
+   */
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) user.password = await Hash.make(user.password)
+  }
+
+  /**
+   * ------------------------------------------------------
+   * Relationships
+   * ------------------------------------------------------
+   * - define User model relationships
+   */
+
+  /**
+   * ------------------------------------------------------
+   * Query Scopes
+   * ------------------------------------------------------
+   */
+
+  /**
+   * ------------------------------------------------------
+   * Misc
+   * ------------------------------------------------------
+   */
+}
---
+++ database/migrations/1653272555138_users.ts
@@ -0,0 +1,33 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+
+      table.string('first_name', 80).notNullable().index('user_first_name_index')
+      table.string('last_name', 80).notNullable().index('user_last_name_index')
+      table.string('username', 50).notNullable().unique().index('username_index')
+      table.string('email', 255).notNullable().unique().index('user_email_index')
+      table.string('password', 180).notNullable()
+
+      table.string('remember_me_token').nullable()
+
+      table.boolean('is_online').notNullable().defaultTo(false)
+      table.boolean('is_blocked').notNullable().defaultTo(false)
+      table.boolean('is_deleted').notNullable().defaultTo(false)
+      /**
+       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true }).notNullable()
+      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('deleted_at', { useTz: true }).defaultTo(null)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
