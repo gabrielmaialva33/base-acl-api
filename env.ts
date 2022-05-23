7650cd644a1ef114986de4be439d02d1d23fbbde
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-05-22 23:11:53 -0300

    chore: configure lucid orm

--- env.ts
+++ env.ts
@@ -15,10 +15,18 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
+  /** App */
   HOST: Env.schema.string({ format: 'host' }),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
   APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  /** Database */
+  PG_HOST: Env.schema.string({ format: 'host' }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
+
 })
