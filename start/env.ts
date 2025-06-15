fa4dab4095298d7c2554e3be0f8e7102afee5e74
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-06-15 16:18:08 -0300

    chore(env): add `.env.test` for testing environment setup
    Included a new `.env.test` file with dummy configurations for testing environments. Added `SESSION_DRIVER` schema validation in `start/env.ts` to support test-specific session settings. Improves testing setup and configuration management.

--- start/env.ts
+++ start/env.ts
@@ -22,6 +22,14 @@ export default await Env.create(new URL('../', import.meta.url), {
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+
+  /*
+  |----------------------------------------------------------
+  | Variables for configuring session package
+  |----------------------------------------------------------
+  */
+  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
+
   /*
   |----------------------------------------------------------
   | Variables for configuring database connection
