07d9f12ea363237c1812ca496e6006059fa22a48
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-06-15 16:18:19 -0300

    feat(kernel): register session and shield middlewares
    Added `@adonisjs/session` and `@adonisjs/shield` middlewares in `start/kernel.ts`. This enables session management and security features such as CSRF protection and HTTP security headers. Enhances application security and user session handling.

--- start/kernel.ts
+++ start/kernel.ts
@@ -36,6 +36,8 @@ router.use([
   () => import('@adonisjs/core/bodyparser_middleware'),
   () => import('@adonisjs/auth/initialize_auth_middleware'),
   () => import('#middleware/detect_user_locale_middleware'),
+  () => import('@adonisjs/session/session_middleware'),
+  () => import('@adonisjs/shield/shield_middleware')
 ])
 
 /**
