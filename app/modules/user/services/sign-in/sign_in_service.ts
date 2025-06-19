d246678332028444fe3f555144838c67565f39e5
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-06-19 20:33:01 -0300

    refactor(sign-in): update `run` method to include typed `SignInResponse`

--- app/modules/user/services/sign-in/sign_in_service.ts
+++ app/modules/user/services/sign-in/sign_in_service.ts
@@ -2,10 +2,16 @@ import { inject } from '@adonisjs/core'
 
 import JwtAuthTokensService from '#modules/user/services/jwt/jwt_auth_tokens_service'
 import UsersRepository from '#modules/user/repositories/users_repository'
+import User from '#modules/user/models/user'
+import { GenerateAuthTokensResponse } from '#modules/user/services/jwt/jwt_auth_tokens_service'
 
 type SignInRequest = {
   uid: string
   password: string
+}
+
+type SignInResponse = User & {
+  auth: GenerateAuthTokensResponse
 }
 
 @inject()
@@ -15,13 +21,13 @@ export default class SignInService {
     private jwtAuthTokensService: JwtAuthTokensService
   ) {}
 
-  async run({ uid, password }: SignInRequest) {
+  async run({ uid, password }: SignInRequest): Promise<SignInResponse> {
     const user = await this.usersRepository.verifyCredentials(uid, password)
     await user.load('roles')
 
     const auth = await this.jwtAuthTokensService.run({ userId: user.id })
     const userJson = user.toJSON()
 
-    return { ...userJson, auth }
+    return { ...userJson, auth } as SignInResponse
   }
 }
