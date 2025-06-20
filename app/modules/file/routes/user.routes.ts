f7b52057e655e79464c07b3b117e0c57c9d5f9f9
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-06-19 23:49:18 -0300

    feat(routes): add `uploadThrottle` middleware to file upload route

--- app/modules/file/routes/user.routes.ts
+++ app/modules/file/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
+import { uploadThrottle } from '#start/limiter'
 import IPermission from '#modules/permission/interfaces/permission_interface'
 
 const FilesController = () => import('#modules/file/controllers/files.controller')
@@ -8,11 +9,12 @@ router
   .group(() => {
     router
       .post('/upload', [FilesController, 'upload'])
-      .use(
+      .use([
         middleware.permission({
           permissions: `${IPermission.Resources.FILES}.${IPermission.Actions.CREATE}`,
-        })
-      )
+        }),
+        uploadThrottle,
+      ])
       .as('files.upload')
   })
   .use(middleware.auth())
