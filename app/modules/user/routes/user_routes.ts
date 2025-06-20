1b9bf96817bb613dee0e010e3532b5f36d0a73a1
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-06-19 21:06:38 -0300

    feat(users): add permission middleware to user routes for authorization

--- app/modules/user/routes/user_routes.ts
+++ app/modules/user/routes/user_routes.ts
@@ -1,27 +1,57 @@
 import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
-import IRole from '#modules/role/interfaces/role_interface'
+import IPermission from '#modules/permission/interfaces/permission_interface'
 
 const UsersController = () => import('#modules/user/controllers/users_controller')
 
 router
   .group(() => {
-    router.get('/', [UsersController, 'paginate']).as('user.paginate')
+    router
+      .get('/', [UsersController, 'paginate'])
+      .use(
+        middleware.permission({
+          permissions: `${IPermission.Resources.USERS}.${IPermission.Actions.LIST}`,
+        })
+      )
+      .as('user.paginate')
+
     router
       .get('/:id', [UsersController, 'get'])
       .where('id', /^[0-9]+$/)
+      .use(
+        middleware.permission({
+          permissions: `${IPermission.Resources.USERS}.${IPermission.Actions.READ}`,
+        })
+      )
       .as('user.get')
-    router.post('/', [UsersController, 'create']).as('user.create')
+
+    router
+      .post('/', [UsersController, 'create'])
+      .use(
+        middleware.permission({
+          permissions: `${IPermission.Resources.USERS}.${IPermission.Actions.CREATE}`,
+        })
+      )
+      .as('user.create')
+
     router
       .put('/:id', [UsersController, 'update'])
       .where('id', /^[0-9]+$/)
+      .use(
+        middleware.permission({
+          permissions: `${IPermission.Resources.USERS}.${IPermission.Actions.UPDATE}`,
+        })
+      )
       .as('user.update')
-    router.delete('/:id', [UsersController, 'delete']).as('user.delete')
+
+    router
+      .delete('/:id', [UsersController, 'delete'])
+      .use(
+        middleware.permission({
+          permissions: `${IPermission.Resources.USERS}.${IPermission.Actions.DELETE}`,
+        })
+      )
+      .as('user.delete')
   })
-  .use([
-    middleware.auth(),
-    middleware.acl({
-      role_slugs: [IRole.Slugs.USER],
-    }),
-  ])
+  .use(middleware.auth())
   .prefix('/api/v1/users')
