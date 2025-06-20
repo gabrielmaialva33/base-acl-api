4f6bd3ec02e993a6471a2cee973dfcb66326ab1f
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-06-19 21:07:25 -0300

    feat(permissions): create `Permission` model with columns, hooks, and relationships

---
+++ app/modules/permission/models/permission.ts
@@ -0,0 +1,79 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, column, manyToMany } from '@adonisjs/lucid/orm'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+
+import Role from '#modules/role/models/role'
+import User from '#modules/user/models/user'
+
+export default class Permission extends BaseModel {
+  static table = 'permissions'
+
+  /**
+   * ------------------------------------------------------
+   * Columns
+   * ------------------------------------------------------
+   */
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare description: string | null
+
+  @column()
+  declare resource: string
+
+  @column()
+  declare action: string
+
+  @column.dateTime({ autoCreate: true })
+  declare created_at: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updated_at: DateTime
+
+  /**
+   * ------------------------------------------------------
+   * Relationships
+   * ------------------------------------------------------
+   */
+  @manyToMany(() => Role, {
+    pivotTable: 'role_permissions',
+    pivotTimestamps: true,
+  })
+  declare roles: ManyToMany<typeof Role>
+
+  @manyToMany(() => User, {
+    pivotTable: 'user_permissions',
+    pivotTimestamps: true,
+    pivotColumns: ['granted', 'expires_at'],
+  })
+  declare users: ManyToMany<typeof User>
+
+  /**
+   * ------------------------------------------------------
+   * Hooks
+   * ------------------------------------------------------
+   */
+  @beforeCreate()
+  static async generateName(permission: Permission) {
+    if (!permission.name) {
+      permission.name = `${permission.resource}.${permission.action}`
+    }
+  }
+
+  /**
+   * ------------------------------------------------------
+   * Query Scopes
+   * ------------------------------------------------------
+   */
+  static byResource = (query: any, resource: string) => {
+    return query.where('resource', resource)
+  }
+
+  static byAction = (query: any, action: string) => {
+    return query.where('action', action)
+  }
+}
